053e2c8cd6a0119ccdaedce07e355ebbf1bdc2bf
Paolo <vioneverforget@gmail.com>
2022-10-29 09:14:48 +0800

    fixed some styling issues && added redirection to projects

---
+++ index.js
@@ -0,0 +1,15 @@
+const primath = document.getElementById("primath");
+const umbella = document.getElementById("umbella");
+const calculator = document.getElementById("bas-cal");
+
+primath.addEventListener("click", () => {
+    window.open("https://primath.netlify.app/", "_blank");
+})
+
+umbella.addEventListener("click", () => {
+    window.open("https://github.com/CoderTofu/Ummbela", "_blank");
+})
+
+calculator.addEventListener("click", () => {
+    window.open("https://github.com/CoderTofu/Basic-Java-Calculator", "_blank");
+})
